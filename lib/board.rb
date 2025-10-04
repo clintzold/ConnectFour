44427f6ee81b3266b2545883b325a0bb6d783a4f
Clint Zold <214383878+clintzold@users.noreply.github.com>
2025-10-03 21:50:22 -0600

    delete graph and related methods

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,10 @@
 #lib/board.rb
 require 'pry-byebug'
 class Squares
-  attr_accessor :id, :mark, :adj
+  attr_accessor :id, :mark
 
     def initialize
-      @mark = '#'
-      @adj = []
+      @mark = ' '
     end
 
 
@@ -13,9 +12,6 @@ class Squares
       @mark = mark
     end
 
-    def add_neighbour(node)
-      @adj << node unless @adj.include?(node)
-    end
 end
 
 class Board
@@ -39,34 +35,8 @@ class Board
     return squares
   end
 
-  def add_edge(node_one, node_two)
-    node_one.add_neighbour(node_two)
-    node_two.add_neighbour(node_one)
-  end
 
-  def build_adjacency_list
-    @squares.each do |square_key, square_value|
-      neighbours = find_neighbours(square_key)
-      neighbours.each do |neighbour|
-        if neighbour[0] <= 6 && neighbour[1] <= 6 && neighbour[0] >= 0 && neighbour[1] >= 0
-          p neighbour
-          add_edge(square_value, @squares[neighbour])
-        end
-      end
-    end
-  end
 
-  def find_neighbours(square)
-    x = square[0]
-    y = square[1]
-    neighbours = [
-    [x + 1, y], [x - 1, y], [x + 1, y + 1], [x + 1, y - 1], [x - 1, y - 1], [x - 1, y + 1], [x, y + 1], [x, y - 1]]
-  end
 
-  def get_square_by_id(id)
-    @squares.each do |key, value|
-      return value if key == id
-    end
-  end
 
 end
